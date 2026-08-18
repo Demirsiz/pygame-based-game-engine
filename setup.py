c6078d281a3ccf035fb9274ccb143ed2e7618af0
Demirsiz <2007.salih.07@gmail.com>
2026-08-18 23:37:20 +0300

    switched to pygame-ce

--- setup.py
+++ setup.py
@@ -2,9 +2,9 @@ from setuptools import setup, find_packages
 
 setup(
     name="demirsiz_engine",
-    version="0.1",
+    version="0.1.1",
     packages=find_packages(),
     install_requires=[
-        "pygame>=2.0.0",
+        "pygame-ce>=2.4.0",
     ],
 )
