4e8f0f8f3a3d8d1550e8c764534d4a1606b7f02f
Demirsiz <2007.salih.07@gmail.com>
2026-08-19 18:48:39 +0300

    m

--- demirsiz_engine/__init__.py
+++ demirsiz_engine/__init__.py
@@ -1,3 +1,3 @@
 from .classes import Mob, Weapon, Wall, Border, SpriteSheet
-from .functions import circle_collison, rect_collision_direction, sprite_distance, repeat_texture
+from .functions import circle_collision, rect_collision_direction, sprite_distance, repeat_texture
 from .spritegroups import all_sprites, border_lines, solid_objects, mobs, walls, weapons 
